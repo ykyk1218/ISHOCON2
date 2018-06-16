eafa60bdedc46f68698144b3b7532851af741cc8
showwin <showwin_kmc@yahoo.co.jp>
2018-06-16 12:09:48 +0900

    [add] appコンテナにGoを追加

--- docker/start_bench.sh
+++ docker/start_bench.sh
@@ -2,8 +2,8 @@
 service mysql start  # なぜか失敗する(調査中)
 chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
 service mysql start  # 正しく起動
-mysql -u root -pishocon -e 'CREATE DATABASE ishocon2;' && \
-mysql -u root -pishocon -e "CREATE USER ishocon IDENTIFIED BY 'ishocon';" && \
+mysql -u root -pishocon -e 'CREATE DATABASE IF NOT EXISTS ishocon2;' && \
+mysql -u root -pishocon -e "CREATE USER IF NOT EXISTS ishocon IDENTIFIED BY 'ishocon';" && \
 mysql -u root -pishocon -e 'GRANT ALL ON *.* TO ishocon;' && \
 cd /admin && tar -jxvf ishocon2.dump.tar.bz2 && mysql -u root -pishocon ishocon2 < /admin/ishocon2.dump
 
