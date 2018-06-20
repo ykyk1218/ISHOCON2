f456b34d868a31909d8e8e55369ba71281b437f5
Goryudyuma <umaretekyoumade@gmail.com>
2018-06-21 07:17:17 +0900

    php add nginx.conf
    phpを動かすために必要なnginx.confを追加

--- webapp/php/index.php
+++ webapp/php/index.php
@@ -1,5 +1,13 @@
 <?php
 
-require 'flight/Flight.php';
+require 'vendor/autoload.php';
 
+Flight::route('/', function(){
+    echo 'hello world!';
+});
 
+Flight::route('/aa', function(){
+    echo 'hello world!aa';
+});
+
+Flight::start();
